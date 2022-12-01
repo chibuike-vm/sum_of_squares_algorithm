166398602f7bd49f053363f77e95c4919f3a20c8
Chibuike Mba <chibuike.code@gmail.com>
2022-12-01 12:42:54 +0100

    Refactor sum_of_squares.c
    This commit will refactor the computational algorithm for calculating
    the sum of squares of any given natural number using "while loop"
    iteration. The previous commit used "for loop" iteration for the same
    purpose. The "while loop" interation was used to ascertain the
    feasibility of its use in designing a computational algorithm for
    calculating the sum of squares of any given natural number or positive
    integer. The if-else conditional statements were used in order to
    enforce that the user will only enter a natural number or positive
    integer.

--- summing_squares/sum_of_squares.c
+++ summing_squares/sum_of_squares.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
 
 int main(void) {
-	int i, sum = 0, squares;
-	printf("Please enter a natural number: ");
+	int i, j = 1, squares, sum = 0;
+	printf("Please, enter a natural number: ");
 	scanf("%d", &i);
-
-	printf("\nThe squares of the natural number; %d are as follows:", i);
-	for (int j = 1 ; j <= i; j++) {
-		squares = (j * j);
-		sum = sum + squares; // === sum += squares
-		printf("%3d", squares);
+	
+	if (i >= 1) {
+    		printf("\nThe squares of the natural number; %d are as follows:", i);
+		while (j <= i) {
+			squares = (j * j);
+			sum = sum + (j * j);
+			printf("%4d", squares);
+			j++;
+		}	
+		printf("\nThe sum of squares for the natural number; %d is = %d\n", i, sum);
+	} else {
+		puts("Please, kindly enter a positive integer or natural number");
 	}
-	printf("\nThe sum of squares of the natural number; %d is = %d\nC language makes computation easy!", i, sum);
-	putchar('\n');
 	return 0;
 }
 
