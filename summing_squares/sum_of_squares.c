59c4bf0c111ad4f2447898ef0e898a69178f9343
Chibuike Mba <chibuike.code@gmail.com>
2022-12-01 14:54:12 +0100

    Add Conditional Statements to sum_of_squares.c
    This commit will add if-else conditional statements to the computational
    algorithm for calculating sum of squares of any given natural number
    already created in the previous base branch with a "for loop" iteration.
    The conditional statements were used to ensure that users, when
    prompted, will input/enter only natural numbers or positive integers.

--- summing_squares/sum_of_squares.c
+++ summing_squares/sum_of_squares.c
@@ -5,14 +5,19 @@ int main(void) {
 	printf("Please enter a natural number: ");
 	scanf("%d", &i);
 
-	printf("\nThe squares of the natural number; %d are as follows:", i);
-	for (int j = 1 ; j <= i; j++) {
-		squares = (j * j);
-		sum = sum + squares; // === sum += squares
-		printf("%3d", squares);
+	if (i >= 1) {
+		printf("\nThe squares of the natural number; %d are as follows:", i);
+		for (int j = 1 ; j <= i; j++) {
+			squares = (j * j);
+			sum = sum + squares; // === sum += squares
+			printf("%3d", squares);
+		}
+		printf("\nThe sum of squares of the natural number; %d is = %d\nC language makes computation easy!", i, sum);
+		putchar('\n');
+	} else {
+		printf("Please, kindly enter a natural number or positive integer\n");
 	}
-	printf("\nThe sum of squares of the natural number; %d is = %d\nC language makes computation easy!", i, sum);
-	putchar('\n');
+
 	return 0;
 }
 
